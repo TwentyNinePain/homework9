3f9d4df5e9922a35ee353db19e783e49f88ad9fe
TwentyNinePain <nagatopainofrainvillage@gmail.com>
2024-10-11 20:01:20 +0300

    homework9.py

--- homework9.py
+++ homework9.py
@@ -3,7 +3,7 @@ second = input('Введите второе число: ')
 third = input('Введите третье число: ')
 if first == second and second == third:
     print(3)
-elif first == second or second == third:
+elif first == second or second == third or first == third:
     print(2)
 else:
     print(0)
